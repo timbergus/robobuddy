81a391140dab3611b81aa79c491710037bd7a193
Gustavo Muñoz <timbergus@gmail.com>
2020-04-25 21:22:21 +0000

    Old project working.

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
+# Executable.
+EXECUTABLE = robobuddy
+
 # This is the compiler.
-CXX=g++
+CXX = g++
 
 # These are the flags.
-CFLAGS=-std=c++17 -Wall -Wextra
-LDFLAGS=librobotcontrol.so.1
-
-# Executable.
-EXECUTABLE=robobuddy
+CFLAGS = -std=c++17 -Wall -Wextra
+LDFLAGS = librobotcontrol.so.1
 
 all:
-    $(CXX) $(CFLAGS) -l:$(LDFLAGS) main.cpp -o output/$(EXECUTABLE)
-    ./output/$(EXECUTABLE)
+	$(CXX) $(CFLAGS) -l:$(LDFLAGS) main.cpp -o $(EXECUTABLE)
+	./$(EXECUTABLE)
--- src/nmea_parser/src/main.cpp
+++ src/nmea_parser/src/main.cpp
@@ -1,30 +1,37 @@
 // Copyright 2018 Gustavo Muñoz
 
+#include <iostream>
 #include "parsers/gnrmc.h"
 
-int main() {
+int main()
+{
   GNRMC gnrmc;
 
-  FILE *file =
-    fopen("/var/lib/cloud9/projects/nmea-parser/src/samples.txt", "r");
+  FILE *file = fopen("./src/samples.txt", "r");
 
-  if (file == NULL) {
+  if (file == NULL)
+  {
+    std::cout << "File not found." << std::endl;
     return 1;
   }
 
   char line[256];
   char type[] = "GNRMC";
 
-  while (fgets(line, sizeof(line), file) != NULL) {
+  while (fgets(line, sizeof(line), file) != NULL)
+  {
     getType(line, type);
-    if (gnrmc.is_valid_string(line) && strcmp(type, "GNRMC") == 0) {
+
+    if (gnrmc.is_valid_string(line) && strcmp(type, "GNRMC") == 0)
+    {
       gnrmc.parse(line);
-      printf("Lat: %f\t", gnrmc.getLatitude());
-      printf("Lng: %f\t", gnrmc.getLongitude());
-      printf("Time: %s\t", gnrmc.getTime());
-      printf("Date: %s\t", gnrmc.getDate());
-      printf("Speed: %f km/h\n", gnrmc.getSpeed());
-      
+
+      std::cout << "Lat: " << gnrmc.getLatitude() << "\t";
+      std::cout << "Lng: " << gnrmc.getLongitude() << "\t";
+      std::cout << "Time: " << gnrmc.getTime() << "\t";
+      std::cout << "Date: " << gnrmc.getDate() << "\t";
+      std::cout << "Speed: " << gnrmc.getSpeed() << "km/h" << std::endl;
+
       usleep(1000000);
     }
   }
