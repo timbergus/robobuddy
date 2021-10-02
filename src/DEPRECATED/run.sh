e96d478f7bacd9cdb23a85d8ec776d6cf68d9cef
Gustavo Muñoz <timbergus@gmail.com>
2021-10-03 01:33:16 +0200

    Clean code.

--- src/DEPRECATED/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path=./output/nmea-parser
-
-if [ ! -f $path ]; then
-    make && ./output/nmea-parser # Build
-else
-    make clean && make && ./output/nmea-parser # Rebuild
-fi
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,10 +7,10 @@ int main()
     // blink(20, 1);
 
     // read_sample_file("./src/data/samples.txt");
-    
+
     int samples = 20000;
-    
-    std::cout << "Reading " <<  samples << " GPS samples." << std::endl;
+
+    std::cout << "Reading " << samples << " GPS samples." << std::endl;
 
     read_gps(samples);
 
